a30f4c9d4b9ed38e0c7ee3b2e7cfec9a69e8ee5e
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-11-22 11:01:33 +0100

    Dodaj pliki projektów.

---
+++ zapis/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace zapis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            x;
+            try
+            {
+                //Open the File
+                StreamWriter sw = new StreamWriter("C:\\pliki\\nowy.txt", true, Encoding.ASCII);
+
+                //Write out the numbers 1 to 10 on the same line.
+                for (x = 0; x < 10; x++)
+                {
+                    sw.Write(x + ";");
+                }
+
+                //close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+
+
+
+        }
+    }
+}
+
+
